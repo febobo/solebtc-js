6f3926481382e65740bc4ce2abfd8231f6f949f1
on99 <on99@users.noreply.github.com>
2016-02-17 17:00:55 +0800

    Introduce react-router for routing

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import { Router, Route, hashHistory } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('main'));
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App}></Route>
+  </Router>
+, document.getElementById('main'));
