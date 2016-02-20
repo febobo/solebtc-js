37e1f0d852cfc74f8bf5ee69d56948a1834ba4a2
on99 <on99@users.noreply.github.com>
2016-02-20 11:00:57 +0800

    Remove redirect because we can configure nginx to serve browser history

--- js/index.js
+++ js/index.js
@@ -7,25 +7,12 @@ import App from './components/App';
 import Home from './components/pages/Home';
 import Register from './components/pages/Register';
 import NotFound from './components/pages/NotFound';
-import {getQueryByName} from './utils/helper';
-
-function redirectIfNecessary(nextState, replace) {
-  let query = nextState.location.search;
-  let refererID = Number(getQueryByName('referer_id', nextState.location.search));
-  if (refererID) {
-    replace({
-      pathname: '/register',
-      query: {referer_id: refererID},
-      state: {nextPathname: nextState.location.pathname}
-    });
-  }
-}
 
 ReactDOM.render(
   <Provider store={configureStore()}>
     <Router history={browserHistory}>
       <Route component={App}>
-        <Route path='/' component={Home} onEnter={redirectIfNecessary} />
+        <Route path='/' component={Home} />
         <Route path='/register' component={Register} />
         <Route path='*' component={NotFound} />
       </Route>
